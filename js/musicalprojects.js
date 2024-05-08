b93353c87a8795b3768ff4f2a375ee19d44d3b00
Bruce Bai <brbai@ucsc.edu>
2024-05-08 08:56:49 -0700

    testing one tiktok vid

--- js/musicalprojects.js
+++ js/musicalprojects.js
@@ -1,12 +1,31 @@
-const prev = document.getElementById('prev-btn')
-const next = document.getElementById('next-btn')
-const list = document.getElementById('item-list')
-const itemWidth = 150
-const padding = 10
 
-prev.addEventListener('click',()=>{
-  list.scrollLeft -= (itemWidth + padding)
-})
-next.addEventListener('click',()=>{
-  list.scrollLeft += (itemWidth + padding)
-})
+
+
+function main() {
+  url = "https://www.tiktok.com/@bruceonaroll/video/7274143716271525163"
+  endpoint = "https://www.tiktok.com/oembed?url=" + url + "&format=json"
+  html = ""
+  $.ajax({
+    url: endpoint,
+    method: 'GET',
+    success: function(response) {
+      html = response.html;
+
+      tiktok_container = document.getElementById("tiktok-vids");
+      const frame = document.createElement('iframe');
+      frame.style.width= 420;
+      frame.style.height = 345;
+      frame.src = html;
+      tiktok_container.appendChild(frame);
+    },
+    error: function(xhr, status, error) {
+      if (xhr.status === 404) {
+        console.log("The requested resource was not found.");
+      } else {
+        console.log("An error occurred:", error);
+      }
+    }
+});
+}
+
+main();
